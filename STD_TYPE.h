a733b51a59142492f6713dc4df6f84f5ea9c882b
kareemahraf <88798476+kareemahraf@users.noreply.github.com>
2021-08-11 22:10:45 +0200

    Add files via upload

---
+++ STD_TYPE.h
@@ -0,0 +1,26 @@
+/*
+ * STD_TYPE.h
+ *
+ * Created: 12/9/2020 11:43:43 AM
+ *  Author: kareem ashraf
+ */ 
+
+
+#ifndef STD_TYPE_H_
+#define STD_TYPE_H_
+
+/*
+#define unsigned char    uint_8;
+#define unsigned short   uint_16;
+#define unsigned long    uint_32;
+#define unsigned float   uf_32;
+#define unsigned double  uf_64;
+
+#define   signed char    sint_8;
+#define   signed short   sint_16;
+#define   signed long    sint_32;
+#define   signed float   sf_32;
+#define   signed double  sd_64;
+*/
+
+#endif /* STD_TYPE_H_ */
